95e2f650c9cbc0b8902101345f9142956f302601
Jens <jensdideriksen1990@gmail.com>
2018-08-23 19:57:44 +0200

    Add project files.

---
+++ LarsBildeAngular/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LarsBildeAngular
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
