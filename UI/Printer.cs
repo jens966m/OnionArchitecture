11cdfac1b0e881fc4b0b5a9541b15e3c73f36450
Jens <jensdideriksen1990@gmail.com>
2018-09-08 15:19:29 +0200

    test

--- UI/Printer.cs
+++ UI/Printer.cs
@@ -14,7 +14,7 @@ namespace BildeConsole
         public Printer(ICustomerService customerService)
         {
             _customerService = customerService;
-
+            // TEST
             DataInit();
         }
 
